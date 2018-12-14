ca522e0758cd79e8dd68f558bc4540d8829e5c53
NNobody7 <ja14marpolsok@gmail.com>
2018-12-14 13:08:33 +0100

    Added self introduction

--- src/main/java/pl/marek/springgitdemo/controller/MyController.java
+++ src/main/java/pl/marek/springgitdemo/controller/MyController.java
@@ -1,6 +1,7 @@
 package pl.marek.springgitdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,11 @@ public class MyController {
     @GetMapping({"/", "/home"})
     public String welcome(){
         return "welcome";
+    }
+    @GetMapping("/me")
+    public String me(Model model){
+        model.addAttribute("name", "Marek");
+        model.addAttribute("surname", "P.");
+        return "me";
     }
 }
