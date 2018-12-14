c1b5b6b343a7764680da103df9582a16798d347e
NNobody7 <ja14marpolsok@gmail.com>
2018-12-14 14:10:38 +0100

    Added car

--- src/main/java/pl/marek/springgitdemo/controller/MyController.java
+++ src/main/java/pl/marek/springgitdemo/controller/MyController.java
@@ -1,6 +1,7 @@
 package pl.marek.springgitdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,4 +10,10 @@ public class MyController {
     public String welcome(){
         return "welcome";
     }
+
+    @GetMapping("/car")
+    public String car(Model model) {
+    model.addAttribute("brand", "Fiat");
+    model.addAttribute("model", "Punto");
+    return "car"; }
 }
