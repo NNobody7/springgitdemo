88ee6ddf9703005891d9caaaca531545143fa729
NNobody7 <ja14marpolsok@gmail.com>
2018-12-14 11:57:51 +0100

    initial commit

---
+++ src/main/java/pl/marek/springgitdemo/SpringGitDemoApplication.java
@@ -0,0 +1,14 @@
+package pl.marek.springgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGitDemoApplication.class, args);
+    }
+
+}
+
